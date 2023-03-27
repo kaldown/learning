1f9e02b7e4dd442fad0043780f725a1e2a381944
kAldown <kaldownb@gmail.com>
2023-03-27 17:10:41 +0400

    146 https://leetcode.com/problems/lru-cache/description/

---
+++ l33tcode/146-LRU-Cache.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.cap = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.cache:
+            if len(self.cache) == self.cap:
+                self.cache.popitem(False)
+        else:
+            self.get(key)
+        self.cache[key] = value
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
