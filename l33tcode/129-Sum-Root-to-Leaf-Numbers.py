9d26293270c5d9c2d649f4f748a00196b3036d8e
kAldown <kaldownb@gmail.com>
2023-03-28 11:13:08 +0400

    129 Add iterative method. DFS => stack

--- l33tcode/129-Sum-Root-to-Leaf-Numbers.py
+++ l33tcode/129-Sum-Root-to-Leaf-Numbers.py
@@ -1,11 +1,10 @@
-# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
 class Solution:
-    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+    def sumNumbers_recursion(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
 
@@ -16,3 +15,19 @@ class Solution:
                 return dfs(node.left, past_num+str(node.val)) + dfs(node.right, past_num+str(node.val))
             return 0
         return dfs(root, "0")
+
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        summa = 0
+        stack = deque([(root, "0")])
+        while stack:
+            node, digit = stack.pop()
+            if node:
+                if not node.left and not node.right:
+                    summa += int(digit + str(node.val))
+                stack.append([node.left, digit + str(node.val)])
+                stack.append([node.right, digit + str(node.val)])
+        return summa
+
